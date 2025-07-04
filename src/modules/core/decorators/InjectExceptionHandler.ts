d0eb8eb45806b133922a6a16617ebda83a1697f5
David200197 <david.alfonso999999@gmail.com>
2025-07-04 10:40:32 -0400

    refactor: remove InjectExceptionHandler decorator and related constant for cleaner dependency injection

--- src/modules/core/decorators/InjectExceptionHandler.ts
+++
@@ -1,4 +0,0 @@
-import { inject } from "inversify";
-import { CORE_DI } from "../di/constants";
-
-export const InjectExceptionHandler = () => inject(CORE_DI.EXCEPTION_HANDLER);
--- src/modules/core/di/constants.ts
+++ src/modules/core/di/constants.ts
@@ -1,5 +1,4 @@
 export const CORE_DI = {
-  EXCEPTION_HANDLER: Symbol.for("EXCEPTION_HANDLER"),
   HTTP_CLIENT: Symbol.for("HTTP_CLIENT"),
   CONFIG_SERVICE: Symbol.for("CONFIG_SERVICE"),
 };
